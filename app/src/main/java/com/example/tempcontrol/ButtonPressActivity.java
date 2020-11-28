b547a2ebf20c8981a4834881bebb3ec41fbb0090
Alexander DeVictoria <43824954+adevi002@users.noreply.github.com>
2020-11-28 15:37:18 -0800

    Created new activity

---
+++ app/src/main/java/com/example/tempcontrol/ButtonPressActivity.java
@@ -0,0 +1,14 @@
+package com.example.tempcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonPressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_press);
+    }
+}
