841c15b6d611163808d835dfb8bebbb4ab92a9f8
Alexander DeVictoria <43824954+adevi002@users.noreply.github.com>
2021-02-27 17:05:30 -0800

    Firebase update

---
+++ app/src/main/java/com/example/tempcontrol/Demo.java
@@ -0,0 +1,16 @@
+package com.example.tempcontrol;
+
+public class Demo {
+    private String Temp;
+
+    public Demo() {
+    }
+
+    public String getTemp() {
+        return Temp;
+    }
+
+    public void setTemp(String temp) {
+        Temp = temp;
+    }
+}
