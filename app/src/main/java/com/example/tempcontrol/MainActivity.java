027fd1974bb7fc04466ccd78175d48405d546bc8
Alexander DeVictoria <43824954+adevi002@users.noreply.github.com>
2021-02-27 17:00:09 -0800

    Update MainActivity.java
    Firebase update

--- app/src/main/java/com/example/tempcontrol/MainActivity.java
+++ app/src/main/java/com/example/tempcontrol/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -30,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
     //the database where we post the room settins
     private DatabaseReference PostValues;
+
+    //variable for Text view
+    private TextView retriveB1, retriveB2, retriveB3;
 
     private int RoomNum = 3;
     int testingVal = 0;
@@ -77,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                Log.i("TempApp", "Button 2 being clicked!");
 //                Toast.makeText(getApplicationContext(), "Edit Temp for B2", Toast.LENGTH_SHORT).show();
-                moveToActivityButton();
+                moveToActivityButton2();
             }
         });
 
@@ -88,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                Log.i("TempApp", "Button 3 being clicked!");
 //                Toast.makeText(getApplicationContext(), "Edit Temp for B3", Toast.LENGTH_SHORT).show();
-                moveToActivityButton();
+                moveToActivityButton3();
             }
         });
 
@@ -100,6 +105,9 @@ public class MainActivity extends AppCompatActivity {
         GetValues = FirebaseDatabase.getInstance().getReference("Demo").child("Room Temp" );
         PostValues = FirebaseDatabase.getInstance().getReference("Demo").child("Room Setting");
 
+        retriveB1 = (TextView)findViewById(R.id.B1Current);
+        retriveB2 = (TextView)findViewById(R.id.B2Current);
+        retriveB3 = (TextView)findViewById(R.id.B3Current);
 
         //This is to set the variables in firebase. Run this to reset the room temp.
         int tes = 70;
@@ -156,6 +164,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
+                String room0=dataSnapshot.child("room0").getValue().toString();
+                String room1=dataSnapshot.child("room1").getValue().toString();
+                String room2=dataSnapshot.child("room2").getValue().toString();
+                retriveB1.setText(room0);
+                if(GetValues.elementAt(0) > PostValues.elementAt(0)) {
+                    ImageView imageView = (ImageView) findViewById(R.id.image1);
+                    imageView.setImageResource(R.drawable.ac_on);
+                }
+                else {
+                    ImageView imageView = (ImageView) findViewById(R.id.image1);
+                    imageView.setImageResource(R.drawable.ac_off);
+                }
+
+                retriveB2.setText(room1);
+                if(70 > 60) {
+                    ImageView imageView = (ImageView) findViewById(R.id.image2);
+                    imageView.setImageResource(R.drawable.ac_on);
+                }
+                else {
+                    ImageView imageView = (ImageView) findViewById(R.id.image2);
+                    imageView.setImageResource(R.drawable.ac_off);
+                }
+
+                retriveB3.setText(room2);
+                if(70 > 60) {
+                    ImageView imageView = (ImageView) findViewById(R.id.image3);
+                    imageView.setImageResource(R.drawable.ac_on);
+                }
+                else {
+                    ImageView imageView = (ImageView) findViewById(R.id.image3);
+                    imageView.setImageResource(R.drawable.ac_off);
+                }
 
                 //temp = Integer.parseInt(dataSnapshot.child("room1").getValue().toString());
                 //Log.d("myTag", String.valueOf(temp));
@@ -165,7 +205,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
-
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
                 // [END_EXCLUDE]
             }
         };
@@ -201,7 +241,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
-
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
                 // [END_EXCLUDE]
             }
         };
@@ -223,7 +263,13 @@ public class MainActivity extends AppCompatActivity {
         startActivity(nextActivity);
     }
 
+    private void moveToActivityButton2() {
+        Intent nextActivity = new Intent(MainActivity.this, Button2PressActivity.class);
+        startActivity(nextActivity);
+    }
 
-
-
+    private void moveToActivityButton3() {
+        Intent nextActivity = new Intent(MainActivity.this, Button3PressActivity.class);
+        startActivity(nextActivity);
+    }
 }
